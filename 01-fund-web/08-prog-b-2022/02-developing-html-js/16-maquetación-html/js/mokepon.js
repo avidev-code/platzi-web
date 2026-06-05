e276bc29cc14673160443f9aa3be8f8746263c82
avidev <avidevcode@gmail.com>
2026-06-05 16:53:44 +0200

    clase 20 - Finalizada la lógica para la elección de la mascota usando condicionales en js 'if-else'

--- 01-fund-web/08-prog-b-2022/02-developing-html-js/16-maquetación-html/js/mokepon.js
+++ 01-fund-web/08-prog-b-2022/02-developing-html-js/16-maquetación-html/js/mokepon.js
@@ -1,6 +1,29 @@
-function seleccionarMascotaJugador() {
-  alert('Seleccionaste tu mascota')
+function iniciarJuego() {
+
+  let botonMascotaJugador = document.getElementById('boton-mascota')
+  botonMascotaJugador .addEventListener('click', seleccionarMascotaJugador)
+
+  //let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+  //sectionSeleccionarAtaque.style.display = 'none'
+
+  //let sectionReiniciar = document.getElementById('reiniciar')
+  //sectionReiniciar.style.display = 'none'
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador .addEventListener('click', seleccionarMascotaJugador)
+function seleccionarMascotaJugador() {
+  let inputHipodoge = document.getElementById('hipodoge')
+  let inputCapipepo = document.getElementById('Capipepo')
+  let inputRatigueya = document.getElementById('Ratigueya')
+
+  if (inputHipodoge.checked) {
+    alert('Seleccionaste a Hipodoge 🐶')
+  } else if (inputCapipepo.checked) {
+    alert('Seleccionaste a Capipepo 🫏')
+  } else if (inputRatigueya.checked) {
+    alert('Seleccionaste a Ratigueya 🐀')
+  } else {
+    alert('Para jugar debes seleccionar una mascota')
+  }
+}
+ 
+window.addEventListener('load', iniciarJuego)
