b7b74a7571aff75102d56efea9123bf125a76ada
avidev <avidevcode@gmail.com>
2026-06-05 18:46:14 +0200

    Clase 24 - Hemos ya definido la lógica de los botones de los ataques de nuestro juego.

--- 01-fund-web/08-prog-b-2022/02-developing-html-js/16-maquetación-html/js/mokepon.js
+++ 01-fund-web/08-prog-b-2022/02-developing-html-js/16-maquetación-html/js/mokepon.js
@@ -1,13 +1,19 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego() {
 
   let botonMascotaJugador = document.getElementById('boton-mascota')
   botonMascotaJugador .addEventListener('click', seleccionarMascotaJugador)
 
-  //let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
-  //sectionSeleccionarAtaque.style.display = 'none'
+  let botonFuego = document.getElementById('boton-fuego')
+  botonFuego.addEventListener('click', ataqueFuego)
 
-  //let sectionReiniciar = document.getElementById('reiniciar')
-  //sectionReiniciar.style.display = 'none'
+  let botonAgua = document.getElementById('boton-agua')
+  botonAgua.addEventListener('click', ataqueAgua)
+
+  let botonTierra = document.getElementById('boton-tierra')
+  botonTierra.addEventListener('click', ataqueTierra)
 }
 
 function seleccionarMascotaJugador() {
@@ -32,15 +38,42 @@ function seleccionarMascotaJugador() {
 }
 
 function seleccionarMascotaEnemigo() {
-  let ataqueAleatorio = aleatorio(1,3)
+  let mascotaAleatoria = aleatorio(1,3)
   let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
 
-  if (ataqueAleatorio == 1) {
+  if (mascotaAleatoria == 1) {
     spanMascotaEnemigo.innerHTML = '🦛 Hipodoge'
-  } else if (ataqueAleatorio == 2) {
+  } else if (mascotaAleatoria == 2) {
     spanMascotaEnemigo.innerHTML = '🫏 Capipepo'
   } else {
     spanMascotaEnemigo.innerHTML = '🐀 Ratigueya'
+  }
+}
+
+function ataqueFuego() {
+  ataqueJugador = '🔥FUEGO'
+  ataqueAleatorioEnemigo()
+}
+
+function ataqueAgua() {
+  ataqueJugador = '💧AGUA'
+  ataqueAleatorioEnemigo()
+}
+
+function ataqueTierra() {
+  ataqueJugador = '🌱TIERRA'
+  ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo() {
+  let ataqueAleatorio = aleatorio(1,3)
+
+  if (ataqueAleatorio == 1) {
+    ataqueEnemigo = '🔥FUEGO'
+  } else if (ataqueAleatorio == 2) {
+    ataqueEnemigo = '💧AGUA'
+  } else {
+    ataqueEnemigo = '🌱TIERRA'
   }
 }
 
