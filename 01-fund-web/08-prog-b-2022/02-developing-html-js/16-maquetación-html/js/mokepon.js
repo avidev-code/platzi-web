e7a6530150edcf73361e6718f1bd64e4362afdaf
avidev <avidevcode@gmail.com>
2026-06-06 08:45:39 +0200

    Clase 26 - Terminamos la lógica de combate del juego, usando funciones de combate y resultado.

--- 01-fund-web/08-prog-b-2022/02-developing-html-js/16-maquetación-html/js/mokepon.js
+++ 01-fund-web/08-prog-b-2022/02-developing-html-js/16-maquetación-html/js/mokepon.js
@@ -76,14 +76,30 @@ function ataqueAleatorioEnemigo() {
     ataqueEnemigo = '🌱TIERRA'
   }
 
-  crearMensaje()
+  combate()
 }
 
-function crearMensaje() {
+function combate () {
+  if (ataqueEnemigo == ataqueJugador) {
+    crearMensaje('♾️EMPATE') 
+  } else if (ataqueJugador == '🔥FUEGO' && ataqueEnemigo == '🌱TIERRA') {
+    crearMensaje('🎉GANASTE')
+  } else if (ataqueJugador == '💧AGUA' && ataqueEnemigo == '🔥FUEGO') {
+    crearMensaje('🎉GANASTE')
+  } else if (ataqueJugador == '🌱TIERRA' && ataqueEnemigo == '💧AGUA') {
+    crearMensaje('🎉GANASTE')
+  } else {
+    crearMensaje('🤕PERDISTE')
+    
+  }
+    
+}
+
+function crearMensaje(resultado) {
   let sectionMensajes = document.getElementById('mensajes')
 
   let parrafo = document.createElement('p')
-  parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ' y la mascota del enemigo atacó con ' + ataqueEnemigo
+  parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ' y la mascota del enemigo atacó con ' + ataqueEnemigo + ' - ' + resultado
 
   sectionMensajes.appendChild(parrafo)
 }
